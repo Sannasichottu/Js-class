f2c6799cf10f18e76252f350100a5253d8b1dba9
sannasichottu <sannasichottu@gmail.com>
2023-10-23 21:26:29 +0530

    userinput & loops

---
+++ loops/script.js
@@ -0,0 +1,70 @@
+//loops
+
+
+// for loop
+//while loop
+//do-while loop
+//for-in loop
+//for-of loop
+
+
+// console.log("Number #1")
+// console.log("Number #2")
+// console.log("Number #3")
+// console.log("Number #4")
+// console.log("Number #5")
+// console.log("Number #6")
+// console.log("Number #7")
+// console.log("Number #8")
+// console.log("Number #9")
+// console.log("Number #10")
+
+
+
+// for(initial Expression, condition, step){
+//     //code
+// }
+
+//for loop
+// for( let i=0;  i<10; i++){
+//     console.log("Number #"+i)
+// }
+
+
+// for( let i=10; i>=0;  i--) {
+//     if(i%2!==0){
+//         console.log('Odd Num #'+i)
+//     }   
+// }
+
+
+
+//while loop
+// let i = 10;
+// while(i>=0){
+//     if(i%2!==0){
+//         console.log("While Num#"+i)
+//     }
+//     i--
+// }
+
+//do-while loop
+
+// do{
+//     //code 
+// }
+// while(condition)
+
+
+// let i = 5;
+
+// do {
+//     if(i%2!==0){
+//         console.log("Do-while loop"+i)
+//     }    
+//     i++
+// }while(i>=20)
+
+//for-in 
+//for-of
+
---
+++ user-input/script.js
@@ -0,0 +1,37 @@
+
+//ternary operators
+
+// condition ? ifTrue : ifElse
+
+// const age  = 16;
+
+// const tamil = age >= 18 ? "Adult" : "child"
+
+// console.log(tamil)
+
+
+//user-input
+
+// alert("Hi Coder")
+
+// let boolean = confirm("Welcome coder")
+// console.log(boolean)
+
+// let myName = prompt("Enter your name");
+// console.log(myName)
+
+
+// let myName = prompt("Enter Your name");
+
+// console.log(myName ?? "you didn't enter your name")   //null colasking
+
+let myName = prompt("Enter Your Name")
+if(myName) {
+    console.log(myName)
+    console.log(myName.length)
+}else {
+    console.log("You did not enter your name")
+}
+console.log(myName.trim())
+console.log(myName.trim().length)
+//trim
